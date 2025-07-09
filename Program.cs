b922252b6c72467c55afec61534e5a9498c5d69d
MoTamer24 <motamerfarh@gmail.com>
2025-07-09 21:49:27 +0300

    adding pictures of running program

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@
         static void Main(string[] args)
         {
             BookService.Add(new EBook("1", "Ebook1",2000, 100,"txt"));
-            BookService.Add(new PaperBook("2", "Ebook1",2000, 150,3));
-            BookService.Add(new Demo("3", "Ebook1",2000, 100));
+            BookService.Add(new PaperBook("2", "Ebook2",2000, 150,3));
+            BookService.Add(new Demo("3", "Ebook3",2000, 100));
 
             BookService.Buy("2", 3, "MoTamerFarh@gmail.com", "Dumyat st 23");
+            BookService.Buy("1", 1, "MoTamerFarh@gmail.com", "Dumyat st 23");
+
+           // BookService.Buy("3", 1, "MoTamerFarh@gmail.com", "Dumyat st 23");
             BookService.RemoveBooksOlderThan(10);
             BookService.Buy("2", 3, "MoTamerFarh@gmail.com", "Dumyat st 23");
+            
 
 
 
